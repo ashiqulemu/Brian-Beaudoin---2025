b372f23919d5e8eab3cf59a567eb06f61b882971
ashiqulemu <ashiqulemu.jpi@gmail.com>
2025-02-14 09:01:18 +0600

    dsfdsf

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@
     document.getElementById("navbarSupportedContent").classList.remove('show');
   
    }
+
+
+   var mycls = document.getElementsByClassName('nav-link');
+for (let i = 0; i <= mycls.length; i++) {
+    mycls[i].addEventListener("click", function() {
+        for (var j = 0; j < mycls.length; j++) {
+            mycls[j].classList.remove('active');
+        }
+        this.classList.add('active'); 
+
+    });
+}
       
 
  
